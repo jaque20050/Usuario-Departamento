52465c26740c8ebb9ab1b3ef37f194ad9b0eff3c
jaque20050 <jaque20050@gmail.com>
2022-03-04 10:25:00 -0300

    Tabela Departamento criada / chave estrangeira

--- src/main/java/com/devsuperior/Userdept/entities/Department.java
+++ src/main/java/com/devsuperior/Userdept/entities/Department.java
@@ -1,7 +1,17 @@
 package com.devsuperior.Userdept.entities;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "tb-department")
 public class Department {
 
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
 
 	private String name;
