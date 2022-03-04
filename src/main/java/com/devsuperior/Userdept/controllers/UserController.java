1420aaf1cf31461b8c095a355c427e12755c7fc2
jaque20050 <jaque20050@gmail.com>
2022-03-04 11:17:57 -0300

    PostMapping / Inserir novo usuário

--- src/main/java/com/devsuperior/Userdept/controllers/UserController.java
+++ src/main/java/com/devsuperior/Userdept/controllers/UserController.java
@@ -5,6 +5,8 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -30,4 +32,9 @@ public class UserController {
 		return result;
 	}
 
+	@PostMapping
+	public User inserir(@RequestBody User user) {
+		User result = repository.save(user);
+		return result;
+	}
 }
