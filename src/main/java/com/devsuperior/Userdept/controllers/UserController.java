0605dfee0f6ba4ede1b57e5e7465346cb7998c23
jaque20050 <jaque20050@gmail.com>
2022-03-04 10:56:54 -0300

    Classe Controller criada / GetMapping findAll

--- src/main/java/com/devsuperior/Userdept/controllers/UserController.java
+++ src/main/java/com/devsuperior/Userdept/controllers/UserController.java
@@ -1,10 +1,25 @@
 package com.devsuperior.Userdept.controllers;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.devsuperior.Userdept.entities.User;
+import com.devsuperior.Userdept.repositories.UserRepository;
 
 @RestController
 @RequestMapping(value = "/users")
 public class UserController {
 
+	@Autowired
+	private UserRepository repository;
+
+	@GetMapping
+	public List<User> buscarTodos() {
+		List<User> result = repository.findAll();
+		return result;
+	}
 }
